d98f43bc68487f3bdc1e608fb348e1241c3ebbb6
Natanael Alves <natanalvespr@gmail.com>
2022-02-13 19:08:20 -0300

    configurando data source

--- filmesApp/View/ViewController.swift
+++ filmesApp/View/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
 }
 
