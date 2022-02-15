d880b3919b2be44cac57f57b3dd09db3a0464596
Natanael Alves <natanalvespr@gmail.com>
2022-02-15 08:59:43 -0300

    adicionando o response

--- filmesApp/Model/ResponseModel.swift
+++ filmesApp/Model/ResponseModel.swift
@@ -4,5 +4,29 @@
 //
 //  Created by Natanael Alves Pereira on 14/02/22.
 //
-
+import UIKit
 import Foundation
+
+class ResponseModel: Codable {
+    var page: Int?
+    var results: [FilmeModel]?
+    var totalPages: Int?
+    var totalResults: Int?
+    
+    init (page: Int?, results: [FilmeModel]?, totalPages: Int? , totalResults: Int?) {
+        self.page = page
+        self.results = results
+        self.totalPages = totalPages
+        self.totalResults = totalResults
+    }
+    
+    enum CodingKeys: String, CodingKey{
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+        case page = "page"
+        case results = "results"
+    }
+  
+    
+}
+
