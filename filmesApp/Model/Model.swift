cbf63e7577fb2dfd36af1f51367f78d9e647efd9
Natanael Alves <natanalvespr@gmail.com>
2022-02-17 13:25:04 -0300

    Otimizando a model

--- filmesApp/Model/Model.swift
+++ filmesApp/Model/Model.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+
+struct MoviesData: Decodable {
+    let movies : [Movie]
+    
+    enum CodingKeys: String , CodingKey {
+        case movies = "results"
+    }
+}
+
+struct Movie: Decodable {
+    
+    let title: String?
+    let year: String?
+    let rate: Double?
+    let posterImage: String?
+    let overview: String?
+    
+    enum CodingKeys: String, CodingKey {
+    
+        case title, overview
+        case year = "release_date"
+        case rate = "vote_average"
+        case posterImage = "poster_path"
+    }
+}
+
