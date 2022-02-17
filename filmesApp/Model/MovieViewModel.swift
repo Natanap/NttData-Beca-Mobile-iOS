0df67415ad35849c31b7a2f7b5eee7581759665c
Natanael Alves <natanalvespr@gmail.com>
2022-02-17 13:28:05 -0300

    chamando a classe api

--- filmesApp/Model/MovieViewModel.swift
+++ filmesApp/Model/MovieViewModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+class MovieViewModel {
+    private var apiService = FilmesAPI()
+    var popularMovies = [Movie]()
+    
+    func fetchPopularMoviesData(completion: @escaping () -> ()){
+        
+        apiService.getPopularMoviesData { [weak self] (result) in
+            
+            switch result {
+            case .success (let listOf):
+                self?.popularMovies = listOf.movies
+                completion()
+            case .failure(let error):
+                print ("Error processing json data: \(error)")
+            }
+        }
+    }
+    func numberOfRowsInSection(section: Int) -> Int {
+          if popularMovies.count != 0 {
+              return popularMovies.count
+          }
+          return 0
+      }
+      
+      func cellForRowAt (indexPath: IndexPath) -> Movie {
+          return popularMovies[indexPath.row]
+      }
+  }
